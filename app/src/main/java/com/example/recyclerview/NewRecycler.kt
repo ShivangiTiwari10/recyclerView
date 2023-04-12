49c1e83bb54a51aeded6daadd33d769b111bbbb2
Shivangi <shivangit1111@gmail.com>
2023-04-12 19:44:03 +0530

    feat: view bind

--- app/src/main/java/com/example/recyclerview/NewRecycler.kt
+++ app/src/main/java/com/example/recyclerview/NewRecycler.kt
@@ -2,10 +2,14 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerview.databinding.ActivityNewRecyclerBinding
 
 class NewRecycler : AppCompatActivity() {
+
+    private  lateinit var  binding: ActivityNewRecyclerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_recycler)
+        binding = ActivityNewRecyclerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
