986483fadbb9239c82710106668017e5e1fcf0f5
Shivangi <shivangit1111@gmail.com>
2023-04-12 18:45:36 +0530

    chore: add activity

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.recyclerview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,36 +15,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnNewRecycler.setOnClickListener {
 
-//        val songs = listOf(
-//            "oh mere",
-//            "ye Jawani",
-//            "yo Yo",
-//            "Dj",
-//            "yeee",
-//            "Kesariya",
-//            "Brahmastra",
-//            " Apna Bana ",
-//            "  Heer Ranjha",
-//            "Rajat Nagpal",
-//            "Baarish Mein Tum",
-//            "  Bas Tujhse Pyaar Ho",
-//            "  Thumkeshwari"
-//
-//        )
+            val intent = Intent(this,NewRecycler::class.java)
+            startActivity(intent)
+            Log.d("Exeption", "$intent")
+        }
 
         val songsObjects = mutableListOf<Song>()
-        songsObjects.add(Song("Hello","this is discription"))
-        songsObjects.add(Song("Abcd","this is JioSavan"))
-        songsObjects.add(Song("1235","this is LocalBeats"))
-        songsObjects.add(Song("12.36","this is radio"))
-        songsObjects.add(Song("Lodon","this is sonuNigam"))
-        songsObjects.add(Song("Mumbai","this is imraan"))
-        songsObjects.add(Song("Laxmi","this is Salman"))
-        songsObjects.add(Song("Key","this is shreya"))
+        songsObjects.add(Song("Hello", "this is discription"))
+        songsObjects.add(Song("Abcd", "this is JioSavan"))
+        songsObjects.add(Song("1235", "this is LocalBeats"))
+        songsObjects.add(Song("12.36", "this is radio"))
+        songsObjects.add(Song("Lodon", "this is sonuNigam"))
+        songsObjects.add(Song("Mumbai", "this is imraan"))
+        songsObjects.add(Song("Laxmi", "this is Salman"))
+        songsObjects.add(Song("Key", "this is shreya"))
 
         songList.adapter = MyAdapter(songsObjects)
         songList.layoutManager = LinearLayoutManager(this)
+
+
     }
 
 
---
+++ app/src/main/java/com/example/recyclerview/NewRecycler.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewRecycler : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_recycler)
+    }
+}
