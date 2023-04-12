a53fdbda2b01f1e4e697ac4075fcdcc159d86a1a
Shivangi <shivangit1111@gmail.com>
2023-04-12 16:31:40 +0530

    chore: add activity

---
+++ app/src/main/java/com/example/recyclerview/NewsRecycler.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsRecycler : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_recycler)
+    }
+}
