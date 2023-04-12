db16bc75d906a6fa3a53eae098fabcb2eb9841f6
Shivangi <shivangit1111@gmail.com>
2023-04-12 16:59:59 +0530

    feat: view bind

--- app/src/main/java/com/example/recyclerview/NewsRecycler.kt
+++ app/src/main/java/com/example/recyclerview/NewsRecycler.kt
@@ -2,10 +2,14 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerview.databinding.ActivityNewsRecyclerBinding
 
 class NewsRecycler : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsRecyclerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news_recycler)
+
+        binding = ActivityNewsRecyclerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
