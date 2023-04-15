81c7424ce40b3740334e799f947d3223af25eaf8
Shivangi <shivangit1111@gmail.com>
2023-04-15 12:40:53 +0530

    chore: add comment

--- app/src/main/java/com/example/recyclerview/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/NewsAdapter.kt
@@ -12,22 +12,30 @@ import com.example.recyclerview.model.News
 class NewsAdapter(private val news: ArrayList<News>) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
+//       if layoutManager fails to find suitable view for each item it calls onCreateViewHolder to create view
+//       by the help of viewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.items_for_newrecycler, parent, false)
         return NewsViewHolder(view)
     }
 
+    //         populate items with data
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         holder.textTittle.text = news[position].title
         holder.texDiscribe.text = news[position].description
         holder.profile.setImageResource(news[position].image)
     }
 
+    //      getItemCount method wants- no. of items
+//     or how many items are present in your array
     override fun getItemCount(): Int {
         return news.size
     }
 
+
+    //    it holds the View so views are not created everyTime, so memory can be saved
     class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val textTittle: TextView = itemView.findViewById(R.id.textTitle)
