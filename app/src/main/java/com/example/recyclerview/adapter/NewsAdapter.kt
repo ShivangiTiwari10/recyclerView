6673325c1f82d19d4ddf979df064fa65c605a1d0
Shivangi <shivangit1111@gmail.com>
2023-04-15 22:49:05 +0530

    feat: make interface

--- app/src/main/java/com/example/recyclerview/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/NewsAdapter.kt
@@ -1,58 +1,72 @@
 package com.example.recyclerview.adapter
 
 import android.app.Activity
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recyclerview.NewsDetailActivity
 import com.example.recyclerview.R
 import com.example.recyclerview.model.News
 
-class NewsAdapter( private val news: ArrayList<News>,private var context: Activity) :
+class NewsAdapter(private val newsArrayList: ArrayList<News>, private var context: Activity) :
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-//       if layoutManager fails to find suitable view for each item it calls onCreateViewHolder to create view
+    private lateinit var myListener: onItemClickListner
+
+    interface onItemClickListner {
+
+        fun onItemClicking(position: Int)
+    }
+
+    fun setItemClickListener(listener: onItemClickListner) {
+
+        myListener = listener
+    }
+
+    //  ***  if layoutManager fails to find suitable view for each item it calls onCreateViewHolder to create view
 //       by the help of viewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
 
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.items_for_newrecycler, parent, false)
-        return NewsViewHolder(view)
+        return NewsViewHolder(view,myListener)
     }
 
     //         populate items with data
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        holder.textTittle.text = news[position].title
-//        holder.texDiscribe.text = news[position].description
-        holder.profile.setImageResource(news[position].image)
 
-        holder.itemView.setOnClickListener {
-            val intent = Intent(context, NewsDetailActivity::class.java)
-            intent.putExtra("title",news[position].title)
-            intent.putExtra("discrip",news[position].description)
-            intent.putExtra("image",news[position].image)
-          context.startActivity(intent)
+        val currentItem = newsArrayList[position]
+        holder.textTittle.text = currentItem.title
+        holder.profile.setImageResource(currentItem.image)
 
-        }
+
     }
 
     //      getItemCount method wants- no. of items
 //     or how many items are present in your array
     override fun getItemCount(): Int {
-        return news.size
+        return newsArrayList.size
     }
 
 
     //    it holds the View so views are not created everyTime, so memory can be saved
-    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class NewsViewHolder(itemView: View, listener: onItemClickListner) :
+        RecyclerView.ViewHolder(itemView) {
 
         val textTittle: TextView = itemView.findViewById(R.id.textTitle)
-        val texDiscribe: TextView = itemView.findViewById(R.id.textDiscribe)
+
+        //        val texDiscribe: TextView = itemView.findViewById(R.id.textDiscribe)
         val profile: ImageView = itemView.findViewById(R.id.profile_image)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClicking(absoluteAdapterPosition)
+
+            }
+
+        }
     }
 }
 
