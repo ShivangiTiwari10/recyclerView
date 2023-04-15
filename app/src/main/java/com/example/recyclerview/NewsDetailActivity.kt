2b0865b6b27c871f1cd85444ee23b6ec302adbd3
Shivangi <shivangit1111@gmail.com>
2023-04-15 13:38:55 +0530

    fat: view bind

--- app/src/main/java/com/example/recyclerview/NewsDetailActivity.kt
+++ app/src/main/java/com/example/recyclerview/NewsDetailActivity.kt
@@ -2,10 +2,15 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerview.databinding.ActivityNewsDetailBinding
 
 class NewsDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news_detail)
+
+        binding = ActivityNewsDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
