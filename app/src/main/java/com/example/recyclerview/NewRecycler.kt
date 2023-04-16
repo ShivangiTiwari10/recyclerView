4317b6a18e1f3214cf3f0e3102ddc8a60cefcb50
Shivangi <shivangit1111@gmail.com>
2023-04-16 12:14:06 +0530

    feat: put data to get in other activity

--- app/src/main/java/com/example/recyclerview/NewRecycler.kt
+++ app/src/main/java/com/example/recyclerview/NewRecycler.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,7 +12,6 @@ class NewRecycler : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewRecyclerBinding
     private lateinit var newsArrayList: ArrayList<News>
-    private lateinit var newsAdapter: NewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +21,6 @@ class NewRecycler : AppCompatActivity() {
         supportActionBar?.hide()
 
         val title = arrayOf("Politics", "science", "environment", "Astrology", "Technology")
-        val discription = arrayOf(
-            "Authoritarians Having a Bad Time The Left Anchor hosts discuss recent",
-            "TLMTI’s Stanley Browne Laboratory (SBL), where basic science research is conducted //n" +
-                    "where basic science research is conducted where basic science research is conducted where basic science research is conducted //n" +
-                    "where basic science research is conducted //n where basic science research is conducted //n where basic science research is conducted" +
-                    "where basic science research is conducted ",
-            "Plat a Tree and Save Earth - Sankalp Taru Foundation",
-            "Astronomy Research. Climate Research. Planetary Research",
-            "Cloud-based. SMS. Programas: Prehospital, Desastre, Emergencia Obstetrica."
-        )
 
         val image = intArrayOf(
             R.drawable.image1,
@@ -39,6 +29,14 @@ class NewRecycler : AppCompatActivity() {
             R.drawable.floura,
             R.drawable.environment
         )
+        val discription = arrayOf(
+          getString(R.string.discriptions),
+          getString(R.string.discriptions),
+          getString(R.string.discriptions),
+          getString(R.string.discriptions),
+          getString(R.string.discriptions),
+        )
+
 
 //        to set hav bhav of items inside recyclerView vertically scrolling,horizontally uniform grid
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
@@ -49,12 +47,25 @@ class NewRecycler : AppCompatActivity() {
 
         for (eachIndexes in title.indices) {
 
-            val news = News(title[eachIndexes], discription[eachIndexes], image[eachIndexes])
+            val news = News(title[eachIndexes], image[eachIndexes],discription[eachIndexes])
             newsArrayList.add(news)
 
         }
-        newsAdapter =NewsAdapter(newsArrayList,this)
+        val newsAdapter = NewsAdapter(newsArrayList,this)
         binding.recyclerView.adapter = newsAdapter
+
+        newsAdapter.setOnItemClickListener(object : NewsAdapter.onItemClickListner{
+            override fun onItemClicking(position: Int) {
+                val intent =Intent(this@NewRecycler,NewsDetailActivity::class.java)
+
+                intent.putExtra("title",newsArrayList[position].title)
+                intent.putExtra("description",newsArrayList[position].description)
+                intent.putExtra("image",newsArrayList[position].image)
+                startActivity(intent)
+
+            }
+
+        })
 
 
 //        To set recyclerView  horizontally
